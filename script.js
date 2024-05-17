abb1b334ee28df8b3eeb3b370b83801a22f8f20a
laur <laura.r.brackney@gmail.com>
2024-05-17 12:59:11 -0500

    Use js DOM to change color of squares on mouseover

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 const container = document.querySelector("#container");
-//const line
 
 for (let c = 1; c<=16; c++) {
     const column = document.createElement("div");
-    // column.textContent = "1";
-    // column.style.height = "49px";
-    // column.style.width = "49px";
-    // column.style.border = "1px solid white";
     for (let i = 1; i<=16; i++) {
         const square = document.createElement("div");
         square.textContent = i;
@@ -15,8 +10,16 @@ for (let c = 1; c<=16; c++) {
         square.style.width = "49px";
         square.style.backgroundColor = "blue";
         square.style.border = "1px solid white";
+        square.classList.add("hover");
         column.appendChild(square);
         
     }
     container.appendChild(column);
 }
+
+const hoverC = document.querySelectorAll(".hover");
+hoverC.forEach((squareToColor) => {
+    squareToColor.addEventListener("mouseover", () => {
+        squareToColor.style.backgroundColor = "lightblue";
+    });
+});
