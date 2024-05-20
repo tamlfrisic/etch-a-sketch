1391f9a371d99f56264fcfd1ff1170115c698e4f
tamlfrisic <tamlfrisic@gmail.com>
2024-05-19 19:38:17 -0500

    Solve formatting and grid size issues

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-// 5/18: 
-// - need to figure out how to create the divs without having to 
-//      define the number of pixels.... 
-// - also, use a prompt to assign a value to a variable in the 
-//      nested loop (instead of the 16) a global var(?)
-//
-//
-
 const container = document.querySelector("#container");
 
 let numOfSquares;
-let squareLength;
     
 function getNumOfSquares() {
-    // container.remove();
-    numOfSquares = Number(prompt("How many squares do you wish?"));
-    console.log("You chose: " + numOfSquares + " squares!");
+    numOfSquares = Number(prompt("How many squares would you like?"));
     return numOfSquares;
 }
 
@@ -22,58 +11,19 @@ const getSquares = document.querySelector(".size");
 getSquares.addEventListener("click", () => {
     container.replaceChildren();
     getNumOfSquares();
-    squareLength = 960 / numOfSquares;
-    squareLength -=  (numOfSquares - 2);
     drawGrid();
 })
 
-// function removeGrid() {
-    
-//     if (container.hasChildNodes) { 
-//     const oldGrid = container.querySelectorAll(".hover");
-//     console.log(oldGrid);
-//     // removeGrid();
-//     oldGrid.forEach(square => {
-//         container.remove(square);
-//         console.log("hiii");
-//     });  
-//     } 
-// };
-
-// function clearGrid() {
-//     if (container.hasChildNodes) { 
-//         container.replaceChildren();
-//         // console.log(container.childNodes);
-//         // const oldGrid = container.querySelectorAll(".hover");
-//         // // console.log(oldGrid);
-//         // // removeGrid();
-//         // oldGrid.forEach(function (square) {
-//         //     container.remove(square);
-//         //     // console.log("this should now be an empty nodelist: " + oldGrid);
-//         // });  
-//         } 
-// }
-
 function drawGrid() {
     console.log("it's drawing " + numOfSquares + " this many squares");
-    
-    
-    
-    // console.log(oldSquares);
-   
-    // if (oldSquares.hasChildNodes()) {
-    //     oldSquares.removeChild(oldSquares.firstChild);
-    //     console.log("it works");
-    // }
 
     for (let c = 1; c<=numOfSquares; c++) {
         const column = document.createElement("div");
+        column.classList.add("hover");
         for (let i = 1; i<=numOfSquares; i++) {
             const square = document.createElement("div");
-            square.textContent = i;
-            square.style.textAlign = "center";
-            square.style.height = squareLength + "px";
-            square.style.width = squareLength + "px";
+            // square.textContent = i;
+            // square.style.textAlign = "center";
             square.style.backgroundColor = "black";
             square.style.border = "1px solid azure";
             square.classList.add("hover");
@@ -110,7 +60,7 @@ function drawGrid() {
         return randomVal;
     
     }
-    
+
     function getRandomColor() {
         randomColor = "rgb(" + getRandomVal() + "," + getRandomVal() + "," + getRandomVal() + ")";
         console.log(randomColor);
