c94b310e72562157168ade9aee409a47225fba3a
tamlfrisic <tamlfrisic@gmail.com>
2024-05-21 15:40:27 -0500

    Add shading and limit prompt answers

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const container = document.querySelector("#container");
 let numOfSquares = 5;
     
 function getNumOfSquares() {
-    numOfSquares = Number(prompt("How many squares would you like?"));
-    return numOfSquares;
+    numOfSquares = Number(prompt("How many squares should each side have? Enter a number between 1 and 100."));
+    if ((typeof numOfSquares === "number") &&
+        (numOfSquares > 0) &&
+        (numOfSquares <= 100)) {
+        return numOfSquares;
+    } else if (numOfSquares === 0) {
+        alert("It's OK to cancel. Goodbye!");
+        container.textContent = " byeeeeeee";
+    } else {
+        alert("Hey, that doesn't meet my requirements! Please try again");
+        getNumOfSquares();
+    }  
 }
 
 const getSquares = document.querySelector(".size");
@@ -13,72 +23,69 @@ getSquares.addEventListener("click", () => {
     getNumOfSquares();
     drawGrid();
 })
-drawGrid(numOfSquares);
+
+container.addEventListener("mouseover", (event) => {
+    if (event.target.classList.contains("hover")) {
+        const squareToColor = event.target;
+        event.stopPropagation();
+
+        switch(choice) {
+            case "normal": 
+                squareToColor.style.backgroundColor = "azure";
+                squareToColor.style.opacity = 1.0;
+                break;
+            case "random":
+                squareToColor.style.backgroundColor = getRandomColor();
+                squareToColor.style.opacity = 1.0;
+                break;
+            case "shading":
+                event.stopPropagation();
+                let currentOpacity = squareToColor.style.opacity;
+                let originalColor = squareToColor.style.backgroundColor;
+                if (originalColor == "black") {
+                    squareToColor.style.backgroundColor = "azure";
+                }
+                if ((currentOpacity - 0.1) < 0) {
+                    squareToColor.style.opacity = 1.0;
+                } else {
+                    squareToColor.style.opacity = currentOpacity - 0.1;
+                }
+                break;
+        }
+    }
+});
+
+const colorChoice = document.querySelector("#colors");
+let choice = colorChoice.value;
+colorChoice.addEventListener("change", () => {
+    choice = colorChoice.value;
+});
+
+let randomColor;
+    
+function getRandomVal() {
+    let randomVal = Math.floor(Math.random() * 256);
+    return randomVal;
+}
+
+function getRandomColor() {
+    randomColor = "rgb(" + getRandomVal() + "," + getRandomVal() + "," + getRandomVal() + ")";
+    return randomColor;
+} 
+
 function drawGrid() {
     for (let c = 1; c<=numOfSquares; c++) {
         const column = document.createElement("div");
         column.classList.add("hover");
         for (let i = 1; i<=numOfSquares; i++) {
             const square = document.createElement("div");
-            // square.textContent = i;
-            // square.style.textAlign = "center";
             square.style.backgroundColor = "black";
             square.style.border = "1px solid azure";
             square.classList.add("hover");
             column.appendChild(square);
-            
         }
         container.appendChild(column);
- 
     }
-
-    // hover/color method 1:
-    // addEventListener is only able to add an event to 1 DOM element
-    // we need to loop through the nodelist to add ELs to every element
-    
-    // const hoverC = document.querySelectorAll(".hover");
-    // hoverC.forEach((squareToColor) => {
-    //     squareToColor.addEventListener("mouseover", () => {
-    //         squareToColor.style.backgroundColor = "lightblue";
-    //     });
-    // });
-    
-    // hover/color method 2:
-    // //event delegation
-    // it doesn't need the if statement, because it's already "checking" 
-    // the conditions with the event listener 
-    
-    // use math random to generate a random RGB value (this should probably be a function??)
-    
-    let randomColor;
-    
-    function getRandomVal() {
-        let randomVal = Math.floor(Math.random() * 256);
-        console.log(randomVal);
-        return randomVal;
-    
-    }
-
-    function getRandomColor() {
-        randomColor = "rgb(" + getRandomVal() + "," + getRandomVal() + "," + getRandomVal() + ")";
-        console.log(randomColor);
-        return randomColor;
-    } 
-    
-    let opacidad = 1.0;
-    container.addEventListener("mouseover", (event) => {
-        const squareToColor = event.target;
-            squareToColor.style.backgroundColor = getRandomColor();
-            squareToColor.style.opacity = opacidad - 0.1;
-            console.log("opacity = " + opacidad);
-    });
-    
-    // allow the grid to return to its original color (cool but against prompt)
-    // container.addEventListener("mouseout", (event) => {
-    //     const squareToColor = event.target;
-    //         squareToColor.style.backgroundColor = "blue";
-    // });
 }
 
-
-
+drawGrid();
